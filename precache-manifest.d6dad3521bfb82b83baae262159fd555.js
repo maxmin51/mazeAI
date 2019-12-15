68873630165b191dcfaf9afc304726ef83752632
Ebrahim <maxmin51>
2019-12-15 12:29:51 +0330

    Updates

---
+++ precache-manifest.d6dad3521bfb82b83baae262159fd555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df06f195f945dbcf55f3e0a5edc5f55",
+    "url": "/mazeAI/index.html"
+  },
+  {
+    "revision": "0fdbdeb842459069b00a",
+    "url": "/mazeAI/static/css/main.36749166.chunk.css"
+  },
+  {
+    "revision": "79765c71771dd196921f",
+    "url": "/mazeAI/static/js/2.4a0a8bdc.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/mazeAI/static/js/2.4a0a8bdc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0fdbdeb842459069b00a",
+    "url": "/mazeAI/static/js/main.f5eaf3dd.chunk.js"
+  },
+  {
+    "revision": "379af8c2d312f4942b5b",
+    "url": "/mazeAI/static/js/runtime-main.056a2e11.js"
+  }
+]);
